35827accca857c4581d24b74636bc2e912a973fc
QINJUNYING819 <qinyangyang200308@163.com>
2026-03-13 11:45:18 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import joblib
+import os
+
+os.environ["GROQ_API_KEY"] = ""
 
 model = joblib.load("foodexp.pkl")
 
@@ -34,6 +37,19 @@ def foodExp():
     q = request.form.get("q", type=float)
     r = model.predict([[q]])
     return(render_template('foodExp.html',r = r[0][0]))
+
+@app.route('/chatbot',methods=['GET', 'POST'])
+def chatbot():
+    return(render_template('chatbot.html')
+
+@app.route('/roe',methods=['GET', 'POST'])
+def roe():
+
+    return(render_template('roe.html')
+
+@app.route('/generalQuestion',methods=['GET', 'POST'])
+def generalQuestion():
+    return(render_template('generalQuestion.html')
 
 if __name__ == '__main__':
     app.run()
